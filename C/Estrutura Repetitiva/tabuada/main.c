a7412a477fc4320e61d2cad6fec79824206baf05
GabrielSn <gsngabriel7@gmail.com>
2026-05-27 18:21:16 -0300

    Add files via upload

---
+++ C/Estrutura Repetitiva/tabuada/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int N, i, produto;
+
+    printf("Deseja a taboada de qual numero? ");
+    scanf("%d", &N);
+
+    for (i = 1; i <= 10; i++){
+        produto = N * i;
+        printf("%d x %d = %d\n", N,i, produto);
+    }
+}
