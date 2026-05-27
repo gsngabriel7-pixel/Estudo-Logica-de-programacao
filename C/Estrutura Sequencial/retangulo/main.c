64778f23d3d8d36b0e1a0deaf27001c330f21fcb
GabrielSn <gsngabriel7@gmail.com>
2026-05-27 17:26:24 -0300

    Add files via upload

---
+++ C/Estrutura Sequencial/retangulo/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double base, altura, area, perimetro, diagonal;
+
+    printf("Base do retangulo: ");
+    scanf("%lf", &base);
+
+    printf("Altura do retangulo: ");
+    scanf("%lf", &altura);
+
+    area = base * altura;
+    perimetro = 2 * base + 2 * altura;
+    diagonal = sqrt(base * base + altura * altura);
+
+    printf("AREA = %.4lf\n", area);
+    printf("Perimetro = %.4lf\n", perimetro);
+    printf("Diagonal = %.4lf\n", diagonal);
+
+
+
+
+
+
+
+}
