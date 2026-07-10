fcd3aef05a9082ea2692462595f2dde39da93515
GabrielSn <gsngabriel7@gmail.com>
2026-07-10 13:52:13 -0300

    Add files via upload

---
+++ C#/Estrutura Condicional/Menor de três/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+CultureInfo CI = CultureInfo.InvariantCulture;
+
+int a,b,c,menor;
+
+System.Console.WriteLine( "Primeiro valor: ");
+a = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine( "Segundo valor: ");
+b = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine( "Terceiro valor: ");
+c = int.Parse(Console.ReadLine());
+
+if ( a < b && a < c) {
+    menor = a;
+}
+else if (b < c )
+{
+    menor = b;
+}
+else
+{
+    menor = c;
+
+}
+
+System.Console.WriteLine( "Menor = " + menor);
---
+++ C#/Estrutura Sequencial/Retangulo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+CultureInfo CI = CultureInfo.InvariantCulture;
+
+double largura, altura, area, perimetro, diagonal;
+
+Console.Write("Base do retangulo: ");
+largura = double.Parse(Console.ReadLine(), CI);
+
+Console.Write("Altura do retangulo: ");
+altura = double.Parse(Console.ReadLine(), CI);
+
+area = largura * altura;
+perimetro = 2 * largura + 2 * altura;
+diagonal = Math.Sqrt(largura * largura + altura * altura);
+
+Console.WriteLine("Area = " + area.ToString("F4",CI ));
+Console.WriteLine("Perimetro = " + perimetro.ToString("F4",CI ));
+Console.WriteLine("diagonal = " + diagonal.ToString("F4",CI ));
